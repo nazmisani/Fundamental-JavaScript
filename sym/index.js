8db0cadbcadfce906153648b269fd13cfd931840
nazmisani <nazminouval@gmail.com>
2025-05-05 00:19:55 +0700

    Sym Finish

--- sym/index.js
+++ sym/index.js
@@ -1,5 +1,20 @@
-function sym(args) {
-  return args;
+// function sym(...args) {
+//   return args.reduce((acc, curr) => [
+//     ...acc,
+//     ...curr.filter((item) => !acc.includes(item) && !curr.includes(item), []),
+//   ]);
+// }
+
+// console.log(sym([1, 2, 3], [5, 2, 1, 4]));
+
+function sym(...args) {
+  return args.reduce(
+    (acc, curr) => [
+      ...acc.filter((item) => !curr.includes(item)),
+      ...curr.filter((item) => !acc.includes(item)),
+    ],
+    []
+  ); // Inisialisasi accumulator sebagai array kosong
 }
 
-sym([1, 2, 3], [5, 2, 1, 4]);
+console.log(sym([1, 2, 3], [5, 2, 1, 4]));
