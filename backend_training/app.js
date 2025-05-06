813abf5eccbc9b99e0dc45131ea8416146d50ff4
nazmisani <nazminouval@gmail.com>
2025-05-06 15:26:20 +0700

    Add nodemon as a dev dependency for improved development workflow

--- backend_training/app.js
+++ backend_training/app.js
@@ -1,2 +1,9 @@
 import express from "express";
 const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+  console.log("Server listening to port 3000");
+});
