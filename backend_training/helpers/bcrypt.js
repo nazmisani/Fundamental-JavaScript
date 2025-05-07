76aa1b50b4a192e4eb1afb248929cd3b54ac2ee1
nazmisani <nazminouval@gmail.com>
2025-05-07 22:40:38 +0700

    Implement password hashing and comparison functions using bcrypt

--- backend_training/helpers/bcrypt.js
+++ backend_training/helpers/bcrypt.js
@@ -1,2 +1,12 @@
 const bcrypt = require("bcryptjs");
 const salt = 10;
+
+const hash_password = (password) => {
+  bcrypt.hashSync(password, salt);
+};
+
+const compare_password = (password, hashed_password) => {
+  bcrypt.compareSync(password, hashed_password);
+};
+
+module.exports = { hash_password, compare_password };
