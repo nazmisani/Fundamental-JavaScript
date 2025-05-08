aeb427fd6b2ab8716901a7c82bea67b292962674
nazmisani <nazminouval@gmail.com>
2025-05-08 23:40:58 +0700

    feat: add routing and views for Home, Login, Register, and BaseLayout components

--- frontend_react/vite-project/src/App.tsx
+++ frontend_react/vite-project/src/App.tsx
@@ -1,10 +1,22 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router";
+import Home from "./views/Home";
+import Login from "./views/Login";
+import Register from "./views/Register";
+import BaseLayout from "./views/BaseLayout";
 
 function App() {
   return (
     <>
-      <BrowserRouter></BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/Register" element={<Register />} />
+          <Route element={<BaseLayout />}>
+            <Route index element={<Home />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
