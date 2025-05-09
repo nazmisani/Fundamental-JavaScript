4ea48b85763bd4ef2dddf492a79944018bd15653
nazmisani <nazminouval@gmail.com>
2025-05-10 00:52:24 +0700

    fix: enhance authentication check in BaseLayout to include current pathname

--- frontend_react/vite-project/src/views/BaseLayout.tsx
+++ frontend_react/vite-project/src/views/BaseLayout.tsx
@@ -1,12 +1,14 @@
-import { Outlet, useNavigate } from "react-router";
+import { Outlet, useLocation, useNavigate } from "react-router";
 import { useEffect } from "react";
 import { toast } from "react-toastify";
+import Home from "./Home";
 
 export default function BaseLayout() {
   const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
-    if (!localStorage.token) {
+    if (!localStorage.token && location.pathname != "/") {
       toast.error("please Login first");
       navigate("/login");
     }
