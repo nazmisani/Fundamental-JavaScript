5cee7d1fe00f3a0588627c34d4dc2c08e02afaf3
nazmisani <nazminouval@gmail.com>
2025-05-09 23:54:42 +0700

    feat: implement multi-step registration form with state management

--- frontend_react/vite-project/src/views/Register.tsx
+++ frontend_react/vite-project/src/views/Register.tsx
@@ -9,8 +9,32 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 export default function Register() {
+  const [step, setStep] = useState(1);
+  const [formData, setFormData] = useState({
+    email: "",
+    username: "",
+    password: "",
+    address: "",
+    phoneNumber: "",
+  });
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  function handleNext() {
+    setStep(2);
+  }
+  function handlePrev() {
+    setStep(1);
+  }
+
   return (
     <div className=" flex justify-center items-center h-screen">
       <Card className="w-[350px]">
@@ -19,28 +43,53 @@ export default function Register() {
           <CardDescription>Daftar Sekarang</CardDescription>
         </CardHeader>
         <CardContent>
-          <Label htmlFor="email">Email</Label>
-          <Input className="mt-2" />
-          <Label className="mt-2" htmlFor="email">
-            Username
-          </Label>
-          <Input className="mt-2" />
-          <Label className="mt-2" htmlFor="email">
-            Phone Number
-          </Label>
-          <Input className="mt-2" />
-          <Label className="mt-2" htmlFor="email">
-            Adress
-          </Label>
-          <Input className="mt-2" />
-          <Label className="mt-2" htmlFor="email">
-            Password
-          </Label>
-          <Input className="mt-2" />
+          {/* STEP 1 */}
+          {step === 1 && (
+            <>
+              <Label htmlFor="email">Email</Label>
+              <Input className="mt-2" />
+              <Label className="mt-2" htmlFor="email">
+                Password
+              </Label>
+              <Input className="mt-2" />
+              <div className="flex justify-end mt-5">
+                <Button variant="outline" onClick={handleNext}>
+                  Next
+                </Button>
+              </div>
+            </>
+          )}
+
+          {/* STEP 2 */}
+          {step === 2 && (
+            <>
+              <Label className="mt-2" htmlFor="email">
+                Username
+              </Label>
+              <Input className="mt-2" />
+              <Label className="mt-2" htmlFor="email">
+                Phone Number
+              </Label>
+              <Input className="mt-2" />
+              <Label className="mt-2" htmlFor="email">
+                Address
+              </Label>
+              <Input className="mt-2" />
+              <div className="flex justify-start mt-5">
+                <Button variant="outline" onClick={handlePrev}>
+                  Back
+                </Button>
+                <Button
+                  className="flex justify-end mt-5"
+                  variant="outline"
+                  onClick={handlePrev}
+                >
+                  Back
+                </Button>
+              </div>
+            </>
+          )}
         </CardContent>
-        <CardFooter>
-          <Button variant="outline">Login</Button>
-        </CardFooter>
       </Card>
     </div>
   );
