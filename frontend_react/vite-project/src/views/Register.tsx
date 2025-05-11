9754365afa29e3037d0c1da3152468a23d5be86a
nazmisani <nazminouval@gmail.com>
2025-05-12 02:25:32 +0700

    feat: implement registration form with multi-step navigation and API integration

--- frontend_react/vite-project/src/views/Register.tsx
+++ frontend_react/vite-project/src/views/Register.tsx
@@ -9,9 +9,13 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
+import { api } from "@/api/api";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router";
 
 export default function Register() {
   const [step, setStep] = useState(1);
+  const navigate = useNavigate();
   const [formData, setFormData] = useState({
     email: "",
     username: "",
@@ -27,6 +31,28 @@ export default function Register() {
     });
   }
 
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(`${api}/apis/add-user`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application.json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await response.json();
+
+      toast.success(`register succesfully`);
+      navigate("/login");
+    } catch (error) {
+      console.log(error);
+      toast.error("registration failed");
+    }
+  }
+
   function handleNext() {
     setStep(2);
   }
@@ -35,57 +61,89 @@ export default function Register() {
   }
 
   return (
-    <div className=" flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center h-screen">
       <Card className="w-[350px]">
         <CardHeader>
           <CardTitle>Register</CardTitle>
           <CardDescription>Daftar Sekarang</CardDescription>
         </CardHeader>
-        <CardContent>
-          <div className="flex justify-center text-xs">
-            <p>step {step} of 2</p>
-          </div>
-          {/* STEP 1 */}
-          {step === 1 && (
-            <>
-              <Label htmlFor="email">Email</Label>
-              <Input className="mt-2" />
-              <Label className="mt-2" htmlFor="email">
-                Password
-              </Label>
-              <Input className="mt-2" />
-              <div className="flex justify-end mt-5">
-                <Button variant="outline" onClick={handleNext}>
-                  Next
-                </Button>
-              </div>
-            </>
-          )}
+        <form onSubmit={handleSubmit}>
+          <CardContent>
+            <div className="flex justify-center text-xs mb-4">
+              <p>Step {step} of 2</p>
+            </div>
 
-          {/* STEP 2 */}
-          {step === 2 && (
-            <>
-              <Label className="mt-2" htmlFor="email">
-                Username
-              </Label>
-              <Input className="mt-2" />
-              <Label className="mt-2" htmlFor="email">
-                Phone Number
-              </Label>
-              <Input className="mt-2" />
-              <Label className="mt-2" htmlFor="email">
-                Address
-              </Label>
-              <Input className="mt-2" />
-              <div className="flex justify-between mt-5">
-                <Button variant="outline" onClick={handlePrev}>
-                  Back
-                </Button>
-                <Button variant="outline">Submit</Button>
-              </div>
-            </>
-          )}
-        </CardContent>
+            {/* STEP 1 */}
+            {step === 1 && (
+              <>
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  className="mt-2"
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                />
+                <Label className="mt-2" htmlFor="password">
+                  Password
+                </Label>
+                <Input
+                  className="mt-2"
+                  type="password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                />
+                <div className="flex justify-end mt-5">
+                  <Button variant="outline" type="button" onClick={handleNext}>
+                    Next
+                  </Button>
+                </div>
+              </>
+            )}
+
+            {/* STEP 2 */}
+            {step === 2 && (
+              <>
+                <Label className="mt-2" htmlFor="username">
+                  Username
+                </Label>
+                <Input
+                  className="mt-2"
+                  name="username"
+                  value={formData.username}
+                  onChange={handleChange}
+                />
+                <Label className="mt-2" htmlFor="phoneNumber">
+                  Phone Number
+                </Label>
+                <Input
+                  className="mt-2"
+                  name="phoneNumber"
+                  value={formData.phoneNumber}
+                  onChange={handleChange}
+                />
+                <Label className="mt-2" htmlFor="address">
+                  Address
+                </Label>
+                <Input
+                  className="mt-2"
+                  name="address"
+                  value={formData.address}
+                  onChange={handleChange}
+                />
+                <div className="flex justify-between mt-5">
+                  <Button variant="outline" type="button" onClick={handlePrev}>
+                    Back
+                  </Button>
+                  <Button variant="outline" type="submit">
+                    Submit
+                  </Button>
+                </div>
+              </>
+            )}
+          </CardContent>
+        </form>
       </Card>
     </div>
   );
