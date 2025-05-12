1409bb373b2b968ab1eccecc5d8feb79acd58ffa
nazmisani <nazminouval@gmail.com>
2025-05-13 01:50:32 +0700

    chatGPT Way

--- vowels/index.js
+++ vowels/index.js
@@ -1,19 +1,36 @@
-function countVowels(string) {
+// MY WAY  +++++++++
+// function countVowels(string) {
+//   let result = 0;
+
+//   let lowString = string.toLowerCase();
+//   //   console.log(lowString);
+
+//   for (let i = 0; i < lowString.length; i++) {
+//     // console.log(lowString[i]);
+
+//     if (
+//       lowString[i] === "a" ||
+//       lowString[i] === "i" ||
+//       lowString[i] === "u" ||
+//       lowString[i] === "e" ||
+//       lowString[i] === "o"
+//     ) {
+//       result += 1;
+//     }
+//   }
+
+//   return console.log(result);
+// }
+
+// ChatGPT Way
+function countVowels(str) {
   let result = 0;
+  const vowels = new Set(["a", "i", "u", "e", "o"]);
 
-  let lowString = string.toLowerCase();
-  //   console.log(lowString);
+  let lowStr = str.toLowerCase();
 
-  for (let i = 0; i < lowString.length; i++) {
-    // console.log(lowString[i]);
-
-    if (
-      lowString[i] === "a" ||
-      lowString[i] === "i" ||
-      lowString[i] === "u" ||
-      lowString[i] === "e" ||
-      lowString[i] === "o"
-    ) {
+  for (let char of lowStr) {
+    if (vowels.has(char)) {
       result += 1;
     }
   }
