7f71002974380933fe9dee8070e6ec4e4e3b53d9
nazmisani <nazminouval@gmail.com>
2025-05-13 01:32:51 +0700

    feat: add vowel counting examples with explanations

---
+++ vowels/index.js
@@ -0,0 +1,11 @@
+countVowels("Hello World");
+// Output: 3
+// Penjelasan: Huruf vokal = 'e', 'o', 'o'
+
+countVowels("JAVASCRIPT");
+// Output: 3
+// Penjelasan: Huruf vokal = 'A', 'A', 'I'
+
+countVowels("Ngoding itu asyik!");
+// Output: 6
+// Penjelasan: Huruf vokal = 'o', 'i', 'i', 'u', 'a', 'i'
