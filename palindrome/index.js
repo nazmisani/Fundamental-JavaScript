f43b97118dbeb0609814e41867d02c97bca63bdf
nazmisani <nazminouval@gmail.com>
2025-05-13 20:53:48 +0700

    chatGPT way

--- palindrome/index.js
+++ palindrome/index.js
@@ -1,16 +1,21 @@
+// My way
+// function isPalindrome(str) {
+//   // code here
+//   let reversed = "";
+
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     reversed += str[i];
+//   }
+
+//   if (reversed === str) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
 function isPalindrome(str) {
-  // code here
-  let reversed = "";
-
-  for (let i = str.length - 1; i >= 0; i--) {
-    reversed += str[i];
-  }
-
-  if (reversed === str) {
-    return true;
-  } else {
-    return false;
-  }
+  return str === str.split("").reverse().join("");
 }
 
 console.log(isPalindrome("madam")); // true
