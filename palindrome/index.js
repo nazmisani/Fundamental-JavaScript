1e8b70044dd5b6666732d6212cc70dbefcfd8d89
nazmisani <nazminouval@gmail.com>
2025-05-13 20:49:03 +0700

    my way

--- palindrome/index.js
+++ palindrome/index.js
@@ -1,5 +1,16 @@
 function isPalindrome(str) {
   // code here
+  let reversed = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+
+  if (reversed === str) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 console.log(isPalindrome("madam")); // true
