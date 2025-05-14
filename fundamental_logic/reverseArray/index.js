792d0eb84ea7084b7b7c0ac739adaeec6333fa62
nazmisani <nazminouval@gmail.com>
2025-05-14 10:54:58 +0700

    feat: update reverseArray function to manually reverse an array

--- fundamental_logic/reverseArray/index.js
+++ fundamental_logic/reverseArray/index.js
@@ -1,6 +1,17 @@
+// function reverseArray(arr) {
+//   // code here
+//   return arr.reverse();
+// }
+
 function reverseArray(arr) {
   // code here
-  return arr.reverse();
+  let result = [];
+
+  for (let i = arr.length; i > 0; i--) {
+    result.push(i);
+  }
+
+  return result;
 }
 
 console.log(reverseArray([1, 2, 3, 4])); // [4, 3, 2, 1]
